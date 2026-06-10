a79b6ab1e551d888f0b1d0ea368f6a7c90825c13
BoFu001 <bofu001@gmail.com>
2026-06-10 16:10:38 +0100

    Update config

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-EQUITYMIND_CORE_URL = os.getenv("EQUITYMIND_CORE_URL", "wss://equitymind.up.railway.app/api/v1/query/stream")
-EQUITYMIND_CORE_KEY = os.getenv("EQUITYMIND_CORE_KEY", "")
 APP_NAME = "EquityMind Web Backend"
+
+# equitymind-core connection
+EQUITYMIND_CORE_URL = os.getenv("EQUITYMIND_CORE_URL")
+EQUITYMIND_CORE_KEY = os.getenv("EQUITYMIND_CORE_KEY")
